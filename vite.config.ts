fcef3ffcd20b33601b6066e784728f37a4802dec
dev-rai1 <devr53247@gmail.com>
2025-06-25 22:37:04 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+base: '/price-wise25/',
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
